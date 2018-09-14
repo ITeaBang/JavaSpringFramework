3e317b5c6212e121eb703b7eab022c361d62e289
ITeaBang <ITeaBang@gmail.com>
2018-09-14 18:36:06 +0900

    factory method pattern, bean factory, creating a bean with xml, ioc, delay generation, dependency Injection

---
+++ spring.tm.180912/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import dao.GoodDao;
+import dao.GoodDaoFactory;
+import vo.GoodVO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// GoodDao 인스턴스 생성
+		// GoodDao dao = new GoodDao();
+		
+		// GoodDao dao =  GoodDaoFactory.create();
+		
+		
+		GenericXmlApplicationContext context = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		GoodDao dao = context.getBean("goodDao", GoodDao.class);
+		System.out.println(dao.hashCode());
+		
+		// 필요한 메소드 호출
+		List<GoodVO> list = dao.getGood();
+		// 메소드 호출 결과 출력
+		for (GoodVO vo : list) {
+			System.out.println(vo);
+		}
+		dao = context.getBean("goodDao", GoodDao.class);
+		System.out.println(dao.hashCode());
+	}
+}
